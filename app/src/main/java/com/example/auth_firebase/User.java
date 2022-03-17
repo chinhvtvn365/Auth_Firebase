8ad36ba73b4b20fdf202639c79737016edc2b222
Chinh Vu <vucongchinh321@gmail.com>
2022-03-17 19:16:36 +0700

    hix

---
+++ app/src/main/java/com/example/auth_firebase/User.java
@@ -0,0 +1,17 @@
+package com.example.auth_firebase;
+
+
+public class User {
+    public String firstName;
+    public String lastName;
+    public String email;
+
+    public User() {
+    }
+
+    public User (String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
